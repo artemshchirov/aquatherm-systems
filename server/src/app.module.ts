295f0127755289a5cd38fadeffcf10f477d71ccd
Artem Shchirov <artemshchirov@gmail.com>
2023-08-25 02:01:53 +0300

    feat(api): init sequelize, mysql with mariadb

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { SequelizeConfigService } from './config/sequelizeConfig.service';
+import { databaseConfig } from './config/configuration';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequelizeConfigService,
+    }),
+    ConfigModule.forRoot({ load: [databaseConfig] }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
