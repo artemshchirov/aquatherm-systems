c113d7fb65b9c47a927f3da72a55bb57bd5ad715
Artem Shchirov <artemshchirov@gmail.com>
2023-08-25 03:09:52 +0300

    feat(api): user create. Add user controller, service, dto

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './users.model';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+  ) {}
+
+  findOne(filter: {
+    where: { id?: string; username?: string; email?: string };
+  }): Promise<User> {
+    return this.userModel.findOne({ ...filter });
+  }
+
+  async create(
+    createUserDto: CreateUserDto,
+  ): Promise<User | { warningMessage: string }> {
+    const user = new User();
+    const existingByUsername = await this.findOne({
+      where: { username: createUserDto.username },
+    });
+    const existingByEmail = await this.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    // TODO: refactor warnings to constants and refactor text messages
+    if (existingByUsername) {
+      return { warningMessage: 'User already exist 1' };
+    }
+
+    if (existingByEmail) {
+      return { warningMessage: 'User already exist 2' };
+    }
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    user.username = createUserDto.username;
+    user.password = hashedPassword;
+    user.email = createUserDto.email;
+
+    return user.save();
+  }
+}
