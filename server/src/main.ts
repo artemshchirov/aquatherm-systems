831e31e6f7bc4ed1bed24a021d716d9ff31c94de
Artem Shchirov <artemshchirov@gmail.com>
2023-08-25 14:01:39 +0300

    feat(api): add Swagger documentation

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(
     session({
       secret: 'keyword',
@@ -14,6 +16,16 @@ async function bootstrap() {
   );
   app.use(passport.initialize());
   app.use(passport.session());
+
+  const config = new DocumentBuilder()
+    .setTitle('Art Box shop')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
---
+++ server/src/users/types/index.ts
@@ -0,0 +1,67 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class LoginUserRequest {
+  @ApiProperty({ example: 'Artem' })
+  username: string;
+
+  @ApiProperty({ example: 'Artem123' })
+  password: string;
+}
+
+export class LoginUserResponse {
+  @ApiProperty({
+    example: {
+      user: {
+        userId: 1,
+        username: 'Artem',
+        password: 'Artem123',
+      },
+    },
+  })
+  user: {
+    userId: number;
+    username: string;
+    password: string;
+  };
+
+  @ApiProperty({ example: 'Logged in' })
+  msg: string;
+}
+
+export class LogoutUserResponse {
+  @ApiProperty({ example: 'Session has ended' })
+  msg: string;
+}
+
+export class LoginCheckResponse {
+  @ApiProperty({ example: 1 })
+  userId: string;
+
+  @ApiProperty({ example: 'Artem' })
+  username: string;
+
+  @ApiProperty({ example: 'artem@gmail.com' })
+  email: string;
+}
+
+export class SignupResponse {
+  @ApiProperty({ example: 1 })
+  id: string;
+
+  @ApiProperty({ example: 'Artem' })
+  username: string;
+
+  @ApiProperty({
+    example: '$2b$10$vPtnNkNUYJ/Sj9NtFrZRJOBapBf/.4kAr4929klYd6yA7gd3Hd09C',
+  })
+  password: string;
+
+  @ApiProperty({ example: 'artem@gmail.com' })
+  email: string;
+
+  @ApiProperty({ example: '2023-08-25T10:33:49.550Z' })
+  updatedAt: string;
+
+  @ApiProperty({ example: '2023-08-25T10:33:49.550Z' })
+  createdAt: string;
+}
