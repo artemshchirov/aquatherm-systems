0354c6db5ddaac8d6bc6ee53f9b94ba1eaf68943
Artem Shchirov <artemshchirov@gmail.com>
2023-08-25 13:15:34 +0300

    feat(api): add auth endpoints login login-check logout

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -5,9 +5,14 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Request,
+  UseGuards,
+  Get,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LocalAuthGuard } from '../auth/local.auth.guard';
+import { AuthenticatedGuard } from '../auth/authenticated.guard';
 
 @Controller('users')
 export class UsersController {
@@ -18,5 +23,24 @@ export class UsersController {
   @Header('Content-type', 'application/json')
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
+  }
+
+  @Post('/login')
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  login(@Request() req) {
+    return { user: req.user, msg: 'Logged in' };
+  }
+
+  @Get('/login-check')
+  @UseGuards(AuthenticatedGuard)
+  loginCheck(@Request() req) {
+    return req.user;
+  }
+
+  @Get('/logout')
+  logout(@Request() req) {
+    req.session.destroy();
+    return { msg: 'Session has ended' };
   }
 }
