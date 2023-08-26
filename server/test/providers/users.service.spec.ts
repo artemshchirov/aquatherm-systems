7a70a6f928e25e29dde1fffea3cd274107227b9c
Artem Shchirov <artemshchirov@gmail.com>
2023-08-26 14:33:11 +0300

    feat(api): add User controller, service e2e tests

---
+++ server/test/providers/users.service.spec.ts
@@ -0,0 +1,55 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Test, TestingModule } from '@nestjs/testing';
+import { databaseConfig } from '../../src/config/configuration';
+import { SequelizeConfigService } from '../../src/config/sequelizeConfig.service';
+import { UsersModule } from '../../src/users/users.module';
+import { User } from '../../src/users/users.model';
+import * as bcrypt from 'bcrypt';
+import { UsersService } from '../../src/users/users.service';
+
+describe('Users service', () => {
+  let app: INestApplication;
+  let usersService: UsersService;
+
+  beforeEach(async () => {
+    const testModule: TestingModule = await Test.createTestingModule({
+      imports: [
+        SequelizeModule.forRootAsync({
+          imports: [ConfigModule],
+          useClass: SequelizeConfigService,
+        }),
+        ConfigModule.forRoot({ load: [databaseConfig] }),
+        UsersModule,
+      ],
+    }).compile();
+
+    usersService = testModule.get<UsersService>(UsersService);
+    app = testModule.createNestApplication();
+    await app.init();
+  });
+
+  afterEach(async () => {
+    await User.destroy({ where: { username: 'Test' } });
+  });
+
+  it('should create user', async () => {
+    const newUser = {
+      username: 'Test',
+      password: 'Test123',
+      email: 'test@gmail.com',
+    };
+
+    const user = (await usersService.create(newUser)) as User;
+
+    const passwordIsValid = await bcrypt.compare(
+      newUser.password,
+      user.password,
+    );
+
+    expect(user.username).toBe(newUser.username);
+    expect(passwordIsValid).toBe(true);
+    expect(user.email).toBe(newUser.email);
+  });
+});
